01dce3fa75787fc7945f35a57a797f0863dfb0a2
Jordan <jcharrison12@gmail.com>
2021-08-02 09:51:50 -0400

    Installed slick carousel, tried to implement in Vue but didn't get the carousel to show I'm so sorry

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import slick from "slick-carousel";
 
+Vue.use(slick);
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <img src="/src/assets/cat1.png" />
+      <img src="/src/assets/cat2.jpeg" />
+    </div>
   </div>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import slick from "slick-carousel";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      sliderOptions: {
+        dots: true,
+        arrows: false,
+        mobileFirst: true,
+        infinite: false,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: true,
+              slidesToShow: 3,
+            },
+          },
+        ],
+      },
+    };
   },
+  created: function () {
+    this.initSlider();
+  },
+  methods: {
+    initSlider() {
+      slick(this.$(".home"), this.sliderOptions);
+    },
+  },
+  updated() {
+    this.$(".home").slick({
+      autoplay: true,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 500,
+          settings: {
+            dots: false,
+            arrows: false,
+            infinite: false,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
+        },
+      ],
+    });
+  },
+  mounted() {},
 };
 </script>
